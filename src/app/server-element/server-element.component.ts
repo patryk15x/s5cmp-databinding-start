138dc2c81a0dbdfbe69c69f5c3bb4315ce47e00a
Patryk Szaruga <szarugapatryk@gmail.com>
2022-07-25 20:46:54 +0200

    Applying alias to @Input

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -8,7 +8,8 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ServerElementComponent implements OnInit {
   server = ElementType.SERVER;
   blueprint = ElementType.BLUEPRINT;
-  @Input() element: ServerElement;
+  // tslint:disable-next-line:no-input-rename
+  @Input('srvElement') element: ServerElement;
 
   constructor() { }
 
